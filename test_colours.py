1d4ab195b8527092a25c7ad70b0495ccf1e7da67
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2024-03-27 17:45:54 +0000

    Creating placeholder for tests (with NotImplementedError)

--- test_colours.py
+++ test_colours.py
@@ -1,17 +1,12 @@
+import my_colours
+import matplotlib
+
+# Hint: matplotlib.colors.to_hex() should give the same as rgb_to_hex(), with rgb values scaled from 0 to 1, instead of 0 to 255
+
 def test_rgb_to_html():
-    import my_colours
-    import matplotlib
-    # define a colour that will enter enough if statements to give 100% coverage overall (i.e. it contains enough special characters)
-    rgb_colour = [234, 12, 189]
-    # use matplotlib to check what the value should be
-    mpl_colour = matplotlib.colors.to_hex([rgb/255 for rgb in rgb_colour])
-    my_colour = my_colours.rgb_to_hex(rgb_colour)
-    assert mpl_colour == my_colour
+    '''Test rgb_to_html function produces expected result'''
+    raise NotImplementedError
 
 def test_rainbow_palette():
-    import my_colours
-    # define a minimal palette which enters all if statements
-    n = 6
-    expected_palette = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff']
-    palette = my_colours.find_rainbow_palette(n)
-    assert palette == expected_palette
+    '''Test rainbow_palette function produces expected result'''
+    raise NotImplementedError

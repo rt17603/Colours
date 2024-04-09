de15db2b21b91df9845c0139bc52b79e43f00cad
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2024-04-09 18:44:24 +0100

    Fixing name of test - html --> hex

--- test_colours.py
+++ test_colours.py
@@ -3,8 +3,8 @@ import matplotlib
 
 # Hint: matplotlib.colors.to_hex() should give the same as rgb_to_hex(), with rgb values scaled from 0 to 1, instead of 0 to 255
 
-def test_rgb_to_html():
-    '''Test rgb_to_html function produces expected result'''
+def test_rgb_to_hex():
+    '''Test rgb_to_hex function produces expected result'''
     raise NotImplementedError
 
 def test_rainbow_palette():
